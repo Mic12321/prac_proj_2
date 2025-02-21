1e99ce14385425403899e62a1b220952abcf84f3
Mic12321 <105850755+Mic12321@users.noreply.github.com>
2025-02-21 18:12:45 +0000

    Added a temp picture and stock management page

--- staff-web/src/pages/StockManagement.tsx
+++ staff-web/src/pages/StockManagement.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import DashboardButton from "../components/DashboardButton";
+import tempImg from "../assets/temp_picture.png";
 
 const StockManagement: React.FC = () => {
   return (
-    <div>
-      <h1>StockManagement</h1>
+    <div className="container d-flex flex-column align-items-center justify-content-center">
+      <h2 className="mb-4 text-center">Stock Management</h2>
+
+      <div className="row g-3 w-100 text-center">
+        <DashboardButton
+          image={tempImg}
+          label="Modify Item Detail Button"
+          displayName="Modify Item Detail"
+          route="/modify-item-detail"
+        />
+        <DashboardButton
+          image={tempImg}
+          label="Modify Menu Button"
+          displayName="Modify Menu"
+          route="/modify-menu"
+        />
+
+        <DashboardButton
+          image={tempImg}
+          label="Modify Ingredient Detail Button"
+          displayName="Modify Ingredient Detail"
+          route="/modify-ingredient-detail"
+        />
+      </div>
     </div>
   );
 };
