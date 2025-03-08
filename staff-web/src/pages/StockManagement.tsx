ca3f5af3a6ac3b36b756d42bfc216aecb8f19276
Mic12321 <105850755+Mic12321@users.noreply.github.com>
2025-03-08 16:25:03 +0000

    Added a NavigateButton

--- staff-web/src/pages/StockManagement.tsx
+++ staff-web/src/pages/StockManagement.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 import DashboardButton from "../components/DashboardButton";
 import tempImg from "../assets/temp_picture.png";
+import NavigateButton from "../components/NavigateButton";
 
 const StockManagement: React.FC = () => {
   return (
-    <div className="container d-flex flex-column align-items-center justify-content-center">
-      <h2 className="mb-4 text-center">Stock Management</h2>
+    <div>
+      <div className="container mt-4">
+        <NavigateButton
+          navUrl="/home"
+          displayName="<- Back to dashboard page"
+        />
+      </div>
+      <div className="container d-flex flex-column align-items-center justify-content-center">
+        <h2 className="mb-4 text-center">Stock Management</h2>
 
-      <div className="row g-3 w-100 text-center">
-        <DashboardButton
-          image={tempImg}
-          label="Search & Edit Items Button"
-          displayName="Search & Edit Items"
-          route="/search-item"
-        />
-        <DashboardButton
-          image={tempImg}
-          label="Modify Menu Button"
-          displayName="Modify Menu"
-          route="/modify-menu"
-        />
+        <div className="row g-3 w-100 text-center">
+          <DashboardButton
+            image={tempImg}
+            label="Search & Edit Items Button"
+            displayName="Search & Edit Items"
+            route="/search-item"
+          />
+          <DashboardButton
+            image={tempImg}
+            label="Modify Menu Button"
+            displayName="Modify Menu"
+            route="/modify-menu"
+          />
+        </div>
       </div>
     </div>
   );
