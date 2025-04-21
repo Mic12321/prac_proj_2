95f0445ec0d7db9e63499e2f48561ba9260e5e82
Mic12321 <105850755+Mic12321@users.noreply.github.com>
2025-04-21 18:49:59 +0100

    Added linked_item_quantity into Category interface

--- staff-web/src/services/categoryService.ts
+++ staff-web/src/services/categoryService.ts
@@ -4,6 +4,7 @@ export interface Category {
   category_id?: number;
   category_name: string;
   category_description: string;
+  linked_item_quantity?: number;
 }
 
 export const getCategories = async (): Promise<Category[]> => {
